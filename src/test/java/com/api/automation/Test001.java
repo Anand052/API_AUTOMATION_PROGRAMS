28e6657661705cb50a2e446bb39b8ea437c0f435
Anand Singh <mailto:anand.singh@gmail.com>
2025-04-08 02:58:56 +0530

    Git Commit done

--- src/test/java/com/api/automation/Test001.java
+++ src/test/java/com/api/automation/Test001.java
@@ -1,4 +1,15 @@
 package com.api.automation;
 
+import io.restassured.RestAssured;
+
 public class Test001 {
+
+    public static void main(String[] args) {
+
+        // Can we call RestAssured, by below line
+
+        // RestAssured
+
+        System.out.println("Hello World");
+    }
 }
